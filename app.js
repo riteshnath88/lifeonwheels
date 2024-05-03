ce77e5db19a457d7a09cc647b24010195e93f362
riteshnath <nathritesh98@gmail.com>
2024-05-03 10:55:18 +0530

    Adding tour and user routes in app.js

--- app.js
+++ app.js
@@ -1,19 +1,73 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     message: 'GET API endpoint',
   });
-});
-
-app.post('/api/v1/tours', (req, res) => {
+};
+const getTour = (req, res) => {
+  res.status(200).json({
+    message: 'GET single record API endpoint',
+  });
+};
+const createTour = (req, res) => {
   res.status(200).json({
     message: 'POST API endpoint',
   });
-});
+};
+const updateTour = (req, res) => {
+  res.status(200).json({
+    message: 'PATCH API endpoint',
+  });
+};
+const deleteTour = (req, res) => {
+  res.status(200).json({
+    message: 'DELETE API endpoint',
+  });
+};
+
+const getAllUsers = (req, res) => {
+  res.status(200).json({
+    message: 'GET API endpoint',
+  });
+};
+const getUser = (req, res) => {
+  res.status(200).json({
+    message: 'GET single record API endpoint',
+  });
+};
+const createUser = (req, res) => {
+  res.status(200).json({
+    message: 'POST API endpoint',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(200).json({
+    message: 'PATCH API endpoint',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(200).json({
+    message: 'DELETE API endpoint',
+  });
+};
+
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`App running on port ${PORT}...`));
