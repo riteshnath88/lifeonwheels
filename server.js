1a84a98631cf53742b4b1863e03244c9eb908cb2
riteshnath <nathritesh98@gmail.com>
2024-05-03 12:59:43 +0530

    Creating mongo atlas connection and tourModel

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then((con) => console.log('DB Connection successful'));
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`App running on port ${PORT}...`));
