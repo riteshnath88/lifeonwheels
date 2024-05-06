4c6d127afe978bcbc7927ba9a63664cefaaaf807
riteshnath <nathritesh98@gmail.com>
2024-05-06 15:55:16 +0530

    Completing exception handling

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down.....');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB).then((con) => console.log('DB Connection successful'));
+mongoose
+  .connect(DB)
+  .then((con) => console.log('DB Connection successful'))
+  .catch((err) => console.log('ERROR'));
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`App running on port ${PORT}...`));
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDELED REJECTION! Shutting down.....');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
