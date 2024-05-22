e66dbb1187b9d438d10235b262a110308824b80f
riteshnath <nathritesh98@gmail.com>
2024-05-22 21:48:48 +0530

    creating the overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,18 @@
-exports.getOverview = (req, res) => {
+const catchAsync = require('../utils/catchAsync');
+const Tour = require('../models/tourModel');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // Get the tour data from the collection
+  const tours = await Tour.find();
+  // Build template
+
+  // Render that template using tour data
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
